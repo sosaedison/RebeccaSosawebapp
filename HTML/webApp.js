91f2061766728739cfb4c231ea17ccfe6f2ba353
sosaedison <sosa_edison@hotmail.com>
2019-04-08 01:07:53 -0500

    messing data retrieval no success

--- HTML/webApp.js
+++ HTML/webApp.js
@@ -8,6 +8,7 @@ var app = new Vue ({
 
     data: {
         map : 'Hello World',
+        reqResults: [],
         cityNames: [],
         count: [],
         country: [],
@@ -16,18 +17,6 @@ var app = new Vue ({
 });
 
 function Init() {
-    /*var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if(req.readyState === 4 && req.status === 200) {
-            let retStuff = req.responseText;
-            console.log(JSON.stringify(retStuff));
-            console.log(retStuff[10]);
-        }
-
-    };
-    req.open("GET",cities, true);
-    req.send(); */
-
     let request = {
         url: cities,
         dataType: "json",
@@ -42,21 +31,21 @@ function toString(data) {
 }
 
 function ParseResults(data) {
-    var i=0;
-    for (i = 0; i < data.results.length; i++)
-        var result = data.results[i];
-        PopulateTable(result.city, result.count, result.country, result.locations);
-}
 
-function PopulateTable(city, count, country, locations) {
-    let table = document.getElementById("map1_table");
-    var table_row = document.createElement("tr");
-    var table_data = document.createElement("td");
-    var stuff = document.createTextNode(city);
+    var i = 0;
+    var x;
+    let f;
+    for(var k in data)
+        console.log(data[k]);
+        i++;
+        if (i == 1)
+            app.reqResults = data[k];
 
-    table_data.append(stuff);
-    table_row.append(table_data);
-    table.append(table_row);
+    for (x in app.reqResults)
+        app.count.push(app.reqResults[x].count);
+
+    for (f = 0; f < app.count.length; f++)
+        console.log(app.count[f]);
 }
 
 
