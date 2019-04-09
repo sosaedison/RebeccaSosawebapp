0a98e99c1fe31bd0aa0a4a3bd974dbe7a37c1801
Sosa Edison <sosa_edison@hotmail.com>
2019-04-08 19:38:28 -0500

    making for countries and outputing to the table

--- HTML/webApp.js
+++ HTML/webApp.js
@@ -38,15 +38,37 @@ function ParseResults(data) {
     for(var k in data)
         console.log(data[k]);
         i++;
-        if (i == 1)
+        if (i === 1)
             app.reqResults = data[k];
+
+
 
     for (x in app.reqResults)
         app.count.push(app.reqResults[x].count);
 
-    for (f = 0; f < app.count.length; f++)
-        console.log(app.count[f]);
+    for(x in app.reqResults)
+        app.cityNames.push(app.reqResults[x].city);
+
+    for(x in app.reqResults)
+        app.locations.push(app.reqResults[x].locations);
+
+    for(x in app.reqResults)
+        app.country.push(app.reqResults[x].country);
+
+    for (f = 0; f<app.cityNames.length;f++)
+        myCreateFunction(app.cityNames[f], app.country[f], app.count[f], app.locations[f])
 }
 
-
+function myCreateFunction(city, country, count, locations) {
+    let table = document.getElementById("map1_table");
+    let row = table.insertRow(0);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    cell1.innerHTML = city;
+    cell2.innerHTML = country;
+    cell3.innerHTML = count;
+    cell4.innerHTML = locations;
+}
 
