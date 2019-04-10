95d975f60d9094a754d29e9de79272e6f3f1f907
Sosa Edison <sosa_edison@hotmail.com>
2019-04-10 13:06:33 -0500

    lat and long are logged when we click

--- HTML/webApp.js
+++ HTML/webApp.js
@@ -16,7 +16,7 @@ var app = new Vue ({
     }
 });
 
-function Init() {
+function GetResults() {
     let request = {
         url: cities,
         dataType: "json",
@@ -65,4 +65,51 @@ function myCreateFunction(city, country, count, locations) {
     cell3.innerHTML = count;
     cell4.innerHTML = locations;
 }
+var mymap = L.map('map1').setView([51.505, -0.09], 13);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#ea40ff',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+var popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(mymap);
+
+// function onMouseUp(e) {
+//     mymap.panTo([51.498827, -0.22645]);
+// }
+
+mymap.on('click', onMapClick);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    //alert("You clicked the map at " + e.latlng);
+
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+    // let LATLNG = e.latlng;
+    // console.log(LATLNG.lat);
+    // console.log(LATLNG.lng);
+}
+
+mymap.on('click', onMapClick);
