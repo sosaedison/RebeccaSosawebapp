73084817bfed1896c0e5c13b8bbe645039278a23
Sosa Edison <sosa_edison@hotmail.com>
2019-04-15 22:09:09 -0500

    STILL DEBUGIN THE MAP BOUNDS GETTER

--- HTML/webApp.js
+++ HTML/webApp.js
@@ -1,6 +1,3 @@
-let countries = "https://api.openaq.org/v1/countries";
-let cities = 'https://api.openaq.org/v1/cities?limit=1000';
-let fetches = 'https://api.openaq.org/v1/fetches';
 let measurements = 'https://api.openaq.org/v1/measurements';
 let latest = 'https://api.openaq.org/v1/latest?coordinates=';
 
@@ -15,30 +12,21 @@ class LatLng {
         return string;
     }
 }
+
+// set max and min zoom to 16 and 9
 var app = new Vue ({
 
     el: '#app',
 
     data: {
-        map : 'Hello World',
         reqResults: [],
         coordinates: [],
         cityNames: [],
         AQ: [],
         country: [],
         locations: []
-    },
-    components: {
-        'map' : Map
     }
 });
-
-Vue.component('map-table', {
-   data: function () {
-       return L.map('map1').setView([44.9544, -93.0913], 10);
-   }
-});
-
 
 function GetResultsLatLng(lat, lng) {
     lat  = lat.toFixed(3);
@@ -100,8 +88,14 @@ function myCreateFunction(city, country, coordinates, locations, values) {
     cell4.innerHTML = locations;
     cell5.innerHTML = values;
 }
+
+/******************************
+ *      MAP STUFF IS HERE     *
+ * ****************************
+ */
 var mymap = L.map('map1').setView([44.9544, -93.0913], 10);
 
+console.log(mymap.getBounds());
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
@@ -111,41 +105,39 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     id: 'mapbox.streets'
 }).addTo(mymap);
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#ea40ff',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
-var popup = L.popup()
-    .setLatLng([51.5, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(mymap);
+// var marker = L.marker([51.5, -0.09]).addTo(mymap);
+// var circle = L.circle([51.508, -0.11], {
+//     color: 'red',
+//     fillColor: '#ea40ff',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(mymap);
+// var polygon = L.polygon([
+//     [51.509, -0.08],
+//     [51.503, -0.06],
+//     [51.51, -0.047]
+// ]).addTo(mymap);
+//
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+// circle.bindPopup("I am a circle.");
+// polygon.bindPopup("I am a polygon.");
+// var popup = L.popup()
+//     .setLatLng([51.5, -0.09])
+//     .setContent("I am a standalone popup.")
+//     .openOn(mymap);
 
-// function onMouseUp(e) {
-//     mymap.panTo([51.498827, -0.22645]);
-// }
 var popup = L.popup();
 
-function onMapClick(e) {
+function onMouseUp(e) {
     popup
-        .setLatLng(e.latlng)
+        //.setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(mymap);
+
         let latlong = e.latlng;
-    // console.log(LATLNG.lat);
-    // console.log(LATLNG.lng);
         GetResultsLatLng(latlong.lat, latlong.lng);
+
 }
 
-mymap.on('click', onMapClick);
+mymap.on('mouseup', onMouseUp);
