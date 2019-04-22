3e853110ee12b09abf6a8910d1ce99db7a989ae7
lewi0151 <45886861+lewi0151@users.noreply.github.com>
2019-04-21 23:39:33 -0500

    Add files via upload
    Can now imput location along with lat and long

--- js.js
+++ js.js
@@ -1,5 +1,25 @@
+	 var limit = '&limit=10000';
 
-		 var limit = '&limit=10000';
+               Mymap= L.map('map').setView([44.0234, -92.4629], 13);
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 16,
+                minZoom:9,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox.streets'
+        }).addTo(Mymap);  
+                               Mymap2= L.map('map2').setView([44.0234, -92.4629], 13);
+
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 16,
+                minZoom:9,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox.streets'
+        }).addTo(Mymap2);  
+
 function GetJSON(url, callback) {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function() {
@@ -11,7 +31,14 @@ function GetJSON(url, callback) {
     req.open("GET", url, true);
     req.send();
 }
-var app = new Vue (
+
+        function pann2(data2) {
+            Mymap2.panTo(new L.LatLng(data2.lat,data2.lon));
+        }
+                function pann1(data1) {
+            Mymap.panTo(new L.LatLng(data1.lat,data1.lon));
+        }
+/*var app = new Vue (
 	{
 
     el: '#maps',
@@ -26,56 +53,121 @@ var app = new Vue (
         locations: []
 	   }
 	});
+*/
 
-function GetResultsLatLng(lat, lng)
+function GetResultsLatLng(lat,lng)
 	{
-		if(document.getElementById('loc')!== null)
-		{
-			var loc1 = document.getElementById('loc');
+    var lat = document.getElementById("lat").value;
+    var lon = document.getElementById("lon").value;
+   
+     var lat2 = document.getElementById("lat2").value;
+    var lon2 = document.getElementById("lon2").value;
+    var loc2 = document.getElementById("loc2").value;
+    var loc1 = document.getElementById("loc").value;
+   var url1= "https://nominatim.openstreetmap.org/search?q=" + loc1+ "&format=json&accept-language=en";
+   var url2= "https://nominatim.openstreetmap.org/search?q=" + loc2+ "&format=json&accept-language=en";
+    var data1;
+    var data2;
+    Mymap2.panTo(new L.LatLng(lat2, lon2));
+    Mymap.panTo(new L.LatLng(lat, lon));
+    var complete_count=0;
+   // data1=[{"lat":lat,"lon":lon}];
+    //data2=[{"lat":lat2,"lon":lon2}];
 
-			var url="https://nominatim.openstreetmap.org/search?q=" + loc1.value + "&format=json&accept-language=en"
-			GetJSON(url, function(data) {
-		        data1 = data;
-		    });
+if((document.getElementById('loc2').value !== ""))
+        {
+    GetJSON(url2, function(data) {
+        complete_count++;
+        console.log(complete_count);
+        data2 = data;
+        if ((document.getElementById('loc').value !== "") && complete_count === 2) {
+            pann2(data2[0]);
+            pann1(data1[0]);
+        }
+                if((document.getElementById('loc').value === "" && complete_count === 1))
+        {
+            pann2(data2[0]);
+        }
+    });
 
-			Mymap= L.map('map').setView([loc1.lon, loc1.lat], 13);
-		}
-		else
-		{
-			Mymap= L.map('map').setView([document.getElementById("lon").value, document.getElementById("lat").value], 13);
-		}
+}
+if(document.getElementById('loc').value !== "" )
+        {
 
-   var center1= (Mymap.getCenter());
+     GetJSON(url1, function(data) {
+        complete_count++;
+         console.log(document.getElementById('loc').value);
+        data1 = data;
+        if ((document.getElementById('loc2').value !== "") && complete_count === 2) {
+            pann1(data1[0]);
+            pann2(data2[0]);
+        }
+        if((document.getElementById('loc2').value === "" && complete_count === 1))
+        {
+            pann1(data1[0]);
+        }
+
+    });
+ /*       var req1 = new XMLHttpRequest();
+    req1.onreadystatechange = function() {
+        if (req1.readyState == 4 && req1.status == 200) {
+           data1 = JSON.parse(req1.response); 
+            console.log(data1[0].lat+","+data1[0].lon);
+            Mymap.panTo(new L.LatLng(data1[0].lat,data1[0].lon));
+            Mymap2.panTo(new L.LatLng(lat2, lon2));
+        }
+    };
+    req1.open("GET", "https://nominatim.openstreetmap.org/search?q=" + loc1+ "&format=json&accept-language=en", true);
+    req1.send();
+     */
+       
+}
+
+   /*var center1= (Mymap.getCenter());
    var centerlatlon1= L.latLng(Mymap.getCenter().lat, Mymap.getCenter().lng);
 var cornerlatlon1=L.latLng(Mymap.getBounds().getSouthWest().lat, Mymap.getBounds().getSouthWest().lng);
-var radius1=Mymap.distance(centerlatlon1,cornerlatlon1);
-    		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-				maxZoom: 16,
-				minZoom:9,
-				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-					'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-					'Imagery � <a href="https://www.mapbox.com/">Mapbox</a>',
-				id: 'mapbox.streets'
-		}).addTo(Mymap);
-   Mymap2= L.map('map2').setView([document.getElementById("lon2").value, document.getElementById("lat2").value], 13);
+var radius1=Mymap.distance(centerlatlon1,cornerlatlon1);*/
+     /*       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 16,
+                minZoom:9,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox.streets'
+        }).addTo(Mymap3);       
+         /*if(document.getElementById('loc2')!== null)
+        {
+            var loc2 = document.getElementById('loc2');
+
+            var url2="https://nominatim.openstreetmap.org/search?q=" + loc2.value + "&format=json&accept-language=en"
+            GetJSON(url2, function(data) {
+                data2 = data;
+            });
+
+            Mymap2= L.map('map2').setView([data2.lon, data2.lat], 13);
+        }
+        else
+        {*/
+         //   Mymap4= L.map('map2').setView([document.getElementById("lon2"), document.getElementById("lat2")], 13);
+        //}
+        /*
    var center2= (Mymap2.getCenter());
    var centerlatlon2= L.latLng(Mymap2.getCenter().lat, Mymap2.getCenter().lng);
 var cornerlatlon2=L.latLng(Mymap2.getBounds().getSouthWest().lat, Mymap2.getBounds().getSouthWest().lng);
-var radius2=Mymap2.distance(centerlatlon2,cornerlatlon2);
+var radius2=Mymap2.distance(centerlatlon2,cornerlatlon2);*/
 
-    		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-				maxZoom: 16,
-				minZoom:9,
-				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-					'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-					'Imagery � <a href="https://www.mapbox.com/">Mapbox</a>',
-				id: 'mapbox.streets'
-		}).addTo(Mymap2);
-		let measurements = 'https://api.openaq.org/v1/measurements';
-let latest = 'https://api.openaq.org/v1/latest?coordinates=';
-	}
-
-
+          /*  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 16,
+                minZoom:9,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox.streets'
+        }).addTo(Mymap4);
+  //      let measurements = 'https://api.openaq.org/v1/measurements';
+//let latest = 'https://api.openaq.org/v1/latest?coordinates=';
+*/
+    }
 function ParseResults(data) {
 
     var i = 0;
@@ -148,7 +240,7 @@ function myCreateFunction(city, country, coordinates, locations, values) {
 
 //var popup = L.popup();
 
-function onMouseUp(e) {
+/*function onMouseUp(e) {
     popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
@@ -157,6 +249,7 @@ function onMouseUp(e) {
         let latlong = e.latlng;
         GetResultsLatLng(latlong.lat, latlong.lng);
 
-}
+}*/
 
 //mymap.on('mouseup', onMouseUp);
+
